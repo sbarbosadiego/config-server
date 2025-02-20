c39202e568fef247ae42b892e23112645a6852d6
Diego Barbosa <diego.barbosa.pro@gmail.com>
2025-02-19 21:13:17 -0300

    Update scriptConfig.sh

--- scriptConfig.sh
+++ scriptConfig.sh
@@ -58,57 +58,57 @@ configurar_Postgres() {
 
 # Realiza o tuning do banco de dados
 tuning_Postgres() {
-    local config_file="/dados/pgsql/$PG_VERSAO/data/postgresql.conf"
+    local postgre_conf="/dados/pgsql/$PG_VERSAO/data/postgresql.conf"
 
     # Verifica se o arquivo existe
-    if [ -f "$config_file" ]; then
+    if [ -f "$postgre_conf" ]; then
         # Executa os comandos como usuário postgres
         sudo -u postgres bash -c "
-            sed -i \"s|^#\\?datestyle = .*|datestyle = 'iso, mdy'|\" \"$config_file\"
-            sed -i \"s|^#\\?standard_conforming_strings = .*|standard_conforming_strings = off|\" \"$config_file\"
-            sed -i \"s|^#\\?listen_addresses = .*|listen_addresses = '*'|\" \"$config_file\"
-            sed -i \"s|^#\\?port = .*|port = 8745|\" \"$config_file\"
-            sed -i \"s|^#\\?tcp_keepalives_idle = .*|tcp_keepalives_idle = 10|\" \"$config_file\"
-            sed -i \"s|^#\\?tcp_keepalives_interval = .*|tcp_keepalives_interval = 10|\" \"$config_file\"
-            sed -i \"s|^#\\?tcp_keepalives_count = .*|tcp_keepalives_count = 10|\" \"$config_file\"
-            sed -i \"s|^#\\?log_destination = .*|log_destination = 'stderr'|\" \"$config_file\"
-            sed -i \"s|^#\\?logging_collector = .*|logging_collector = off|\" \"$config_file\"
-            sed -i \"s|^#\\?log_directory = .*|log_directory = 'log'|\" \"$config_file\"
-            sed -i \"s|^#\\?log_rotation_age = .*|log_rotation_age = 1d|\" \"$config_file\"
-            sed -i \"s|^#\\?log_filename = .*|log_filename = 'postgresql-%a.log'|\" \"$config_file\"
-            sed -i \"s|^#\\?log_rotation_size = .*|log_rotation_size = 20MB|\" \"$config_file\"
-            sed -i \"s|^#\\?log_truncate_on_rotation = .*|log_truncate_on_rotation = on|\" \"$config_file\"
-            sed -i \"s|^#\\?enable_seqscan = .*|enable_seqscan = on|\" \"$config_file\"
-            sed -i \"s|^#\\?enable_partitionwise_join = .*|enable_partitionwise_join = on|\" \"$config_file\"
-            sed -i \"s|^#\\?enable_partitionwise_aggregate = .*|enable_partitionwise_aggregate = on|\" \"$config_file\"
-            sed -i \"s|^#\\?password_encryption = .*|password_encryption = md5|\" \"$config_file\"
-            sed -i \"s|^#\\?default_statistics_target = .*|default_statistics_target = 1000|\" \"$config_file\"
+            sed -i \"s|^#\\?datestyle = .*|datestyle = 'iso, mdy'|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?standard_conforming_strings = .*|standard_conforming_strings = off|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?listen_addresses = .*|listen_addresses = '*'|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?port = .*|port = 8745|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?tcp_keepalives_idle = .*|tcp_keepalives_idle = 10|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?tcp_keepalives_interval = .*|tcp_keepalives_interval = 10|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?tcp_keepalives_count = .*|tcp_keepalives_count = 10|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?log_destination = .*|log_destination = 'stderr'|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?logging_collector = .*|logging_collector = off|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?log_directory = .*|log_directory = 'log'|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?log_rotation_age = .*|log_rotation_age = 1d|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?log_filename = .*|log_filename = 'postgresql-%a.log'|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?log_rotation_size = .*|log_rotation_size = 20MB|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?log_truncate_on_rotation = .*|log_truncate_on_rotation = on|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?enable_seqscan = .*|enable_seqscan = on|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?enable_partitionwise_join = .*|enable_partitionwise_join = on|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?enable_partitionwise_aggregate = .*|enable_partitionwise_aggregate = on|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?password_encryption = .*|password_encryption = md5|\" \"$postgre_conf\"
+            sed -i \"s|^#\\?default_statistics_target = .*|default_statistics_target = 1000|\" \"$postgre_conf\"
         "
-        echo "Configurações de tuning aplicadas com sucesso no arquivo $config_file."
+        echo "Configurações de tuning aplicadas com sucesso no arquivo $postgre_conf."
     else
-        echo "Arquivo $config_file não encontrado. Verifique se o PostgreSQL foi inicializado corretamente."
+        echo "Arquivo $postgre_conf não encontrado. Verifique se o PostgreSQL foi inicializado corretamente."
         exit 1
     fi
 }
 
+# Configura método de conexao local
 configurar_Autenticacao() {
-    local config_file="/dados/pgsql/$PG_VERSAO/data/pg_hba.conf"
+    local pg_hba="/dados/pgsql/$PG_VERSAO/data/pg_hba.conf"
+    local local_auth_line=85
+    local host_auth_line=87
 
-    # Verifica se o arquivo existe
-    if [ -f "$config_file" ]; then
-        # Executa os comandos como usuário postgres
-        sudo -u postgres bash -c "
-            sed -i 's|^local[[:space:]]\+all[[:space:]]\+all[[:space:]]\+peer$|local   all             all                                     trust|' \"$config_file\"
-            sed -i 's|^host[[:space:]]\+all[[:space:]]\+all[[:space:]]\+127\.0\.0\.1/32[[:space:]]\+md5$|host    all             all             127.0.0.1/32            trust|' \"$config_file\"
-        "
-        echo "Configuração de autenticação alterada para 'trust' no arquivo $config_file."
-    else
-        echo "Arquivo $config_file não encontrado. Verifique se o PostgreSQL foi inicializado corretamente."
+    if [ ! -f "$pg_hba" ]; then
+        echo "Arquivo $pg_hba não encontrado. Verifique se o PostgreSQL foi inicializado corretamente."
         exit 1
     fi
+    
+    sudo -u postgres sed -i "${local_auth_line}s/\(.*\)[[:space:]]\+\(md5\|peer\|scram-sha-256\|password\|reject\|ident\|trust\)[[:space:]]*$/\1 trust/" "$pg_hba"
+    sudo -u postgres sed -i "${host_auth_line}s/\(.*\)[[:space:]]\+\(md5\|peer\|scram-sha-256\|password\|reject\|ident\|trust\)[[:space:]]*$/\1 trust/" "$pg_hba"
+
+    echo "Configuração de autenticação alterada para 'trust' no arquivo $pg_hba."
 }
 
-# Funcao para instalar alguns aplicativos uteis na manutencao do servidor
+# Instala utilitarios para manutencao do servidor
 instalar_Utilitarios() {
     dnf install epel-release -y
     dnf install htop -y
@@ -121,7 +121,7 @@ instalar_Utilitarios() {
     dnf update -y
 }
 
-# Configura um diretorio com todos os scripts que podem ser utilizados no servidor
+# Configura diretorio com scripts utilizados na crontab
 configurar_Scripts(){
     cd /
     mkdir util
